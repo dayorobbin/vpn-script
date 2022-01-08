e33d7eb4310ae3a48e7b9ff5afa5b76a33049699
dayorobbin <robbinlead@gmail.com>
2022-01-08 13:23:29 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ fi
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
-MYIP=$(wget -qO- ipinfo.io/ip);
+MYIP=$(wget -qO- icanhazip.com);
 echo "Checking VPS"
 IZIN=$( curl https://raw.githubusercontent.com/dayorobbin/vpn-script/main/ipvps | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
