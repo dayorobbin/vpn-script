a18d3bf93b5138b1e251bce684e82da7e87631e5
dayorobbin <robbinlead@gmail.com>
2022-01-08 15:03:14 +0100

    Update setup#Enda.sh

--- setup#Enda.sh
+++ setup#Enda.sh
@@ -11,7 +11,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
-IZIN=$( curl http://akses.vmess.my.id:81/aksesku | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/dayorobbin/vpn-script/main/aksesku | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
